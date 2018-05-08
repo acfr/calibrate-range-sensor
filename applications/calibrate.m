28193aaa66031968fb307cbd8082e8f9b940110f
James Underwood <j.underwood@acfr.usyd.edu.au>
2018-05-08 18:07:58 +1000

    added licence info at top of each file; directory name in calibrate.m less specific

--- applications/calibrate.m
+++ applications/calibrate.m
@@ -1,9 +1,39 @@
+% This file is part of calibrate-range-sensor, a utility to calibrate extrinsic range sensor offsets
+% Copyright (c) 2011 The University of Sydney
+% All rights reserved.
+%
+% Redistribution and use in source and binary forms, with or without
+% modification, are permitted provided that the following conditions are met:
+% 1. Redistributions of source code must retain the above copyright
+%    notice, this list of conditions and the following disclaimer.
+% 2. Redistributions in binary form must reproduce the above copyright
+%    notice, this list of conditions and the following disclaimer in the
+%    documentation and/or other materials provided with the distribution.
+% 3. Neither the name of the University of Sydney nor the
+%    names of its contributors may be used to endorse or promote products
+%    derived from this software without specific prior written permission.
+%
+% NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE
+% GRANTED BY THIS LICENSE.  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT
+% HOLDERS AND CONTRIBUTORS \"AS IS\" AND ANY EXPRESS OR IMPLIED
+% WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+% MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+% DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+% LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+% CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+% SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR
+% BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+% WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
+% OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN
+% IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+%
+
 %check dependencies
 if exist('bin_load')~=2
     error('Could not find required dependency: comma/csv/examples/bin_load.m - check your path settings')
 end
 reload=1;
-dataFolder='x:/mantis-shrimp/processed/acfr/2015-11-18-grass/lidar-calibration0/'
+dataFolder='/home/data/my-calibration-data'
 
 featureFiles=dir([dataFolder,'/*features.bin']);
 nSensors=length(featureFiles);
@@ -77,7 +107,7 @@ costfunctions{4} = 'GeometricCostOfUnconstrainedPlane';
 costfunctions{5} = 'GeometricCostOfUnconstrainedPlane';
 costfunctions{6} = 'GeometricCostOfUnconstrainedPlane';
 costfunctions{7} = 'GeometricCostOfUnconstrainedPlane';
-%costfunctions{7} = Ignore;
+costfunctions{7} = Ignore;
 
 options = optimset( 'Algorithm','active-set','MaxFunEvals', 2000, 'MaxIter', 1000, 'Display','iter', 'TolFun', 1e-10, 'TolX', 1e-10);
 
